e29d48bd071c5b1a5f3f1bf8062ea8d3b56dfde8
facct25pd <facct25pd@gmail.com>
2025-06-19 07:49:05 +0000

    Deploying to gh-pages from @ facct25pd/facct25pd.github.io@d6b877ec478966350e4d7d7fee2708ef2ad3a1cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,27 +3,13 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
