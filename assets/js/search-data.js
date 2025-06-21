2814f858e56f150e9514ac716416208b8777c350
yananlong <yanan.long439@gmail.com>
2025-06-21 11:09:41 +0000

    Deploying to gh-pages from @ facct25pd/facct25pd.github.io@7fa29873a56b5935af81188b9ea7d6dcb1bb34ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-taking-stock-at-facct-insights-through-participatory-design",
-    title: "Taking Stock at FAccT: Insights through Participatory Design",
+    id: "nav-acm-facct-2025",
+    title: "ACM FAccT 2025",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
